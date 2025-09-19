58d8e9429da60e04e3c41532260494fde8b4a594
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-18 20:47:27 -0400

    Corrigido problema de scroll horizontal no menu mobile

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5329, hash: '98ae5c4e7bd94715c001a2c662149547bbb80402be90fd11c4e1d4e454bdf7e5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5422, hash: 'd940e0017842dd3c65d5cf06328acf10f463b65c9eadae8c50780afafedbad9c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 63452, hash: '4c8ef1e8ca2dd7e7dc3a45f9479d9c04835ddd7ed4a7093f323fbbb28397ac3c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-4OMVEM3Y.css': {size: 85063, hash: '5J9ugc0P8GI', text: () => import('./assets-chunks/styles-4OMVEM3Y_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5326, hash: '8a67887d5c35dcdae18c2a376f529b980723466e16a3905af7a435bbfc0ea514', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5422, hash: '995299d643b9da7fb78ed6958e7a79b9af333773bd5f98c9ea32828153c225a2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 63474, hash: '0b39bafa6015cc1ea2e0cc28215b21355cab01558986325b5a518252e2310036', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-XWVDB3V2.css': {size: 85060, hash: 'kZGlEfkoqVw', text: () => import('./assets-chunks/styles-XWVDB3V2_css.mjs').then(m => m.default)}
   },
 };
