1037b1465e630c7b5d6511bc71a50b74001e4343
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-20 15:38:15 -0400

    update for deploy on Github Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5326, hash: '5b1063bdd070f287f641f28325dcd19110291340ad521a711d19a64ec8081e66', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5422, hash: '15754cb71e6e9c41519cc59d3dabdf9eada4cf83c6313e85f92f428c591c61ff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 64087, hash: '0c1bce150d5b40a9fc34f0f0a3c2e247f166e267b1dc81c2788196c9c2cbea45', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-XWVDB3V2.css': {size: 85060, hash: 'kZGlEfkoqVw', text: () => import('./assets-chunks/styles-XWVDB3V2_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5232, hash: '078d1abd2829d0a87a0b860ce157890faa8fc1731b643204c919f7d176967fa3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5422, hash: 'b54a398934a863dd092dbd58da4368c5ae5e2fa8f4a15f1aa6a8865c426e8bca', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 65292, hash: '2805788c1ab7e2aa3958a640d1da278741d72ae6b04cb327761a1fee9fada3b2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-B4SNAQ6I.css': {size: 84966, hash: 'b3mpnjCDpdY', text: () => import('./assets-chunks/styles-B4SNAQ6I_css.mjs').then(m => m.default)}
   },
 };
