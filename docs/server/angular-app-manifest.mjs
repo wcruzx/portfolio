c396b25a7df95e893ce163ebe1f07c42306c7df7
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-21 12:07:01 -0400

    build: atualização do build para GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,18 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/portfolio/',
+  baseHref: 'C:/Program Files/Git/portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/portfolio"
+    "route": "/Program%20Files/Git/portfolio"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5232, hash: '078d1abd2829d0a87a0b860ce157890faa8fc1731b643204c919f7d176967fa3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5422, hash: 'b54a398934a863dd092dbd58da4368c5ae5e2fa8f4a15f1aa6a8865c426e8bca', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 65292, hash: '2805788c1ab7e2aa3958a640d1da278741d72ae6b04cb327761a1fee9fada3b2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5252, hash: 'fd84451c5551aad766e3a3b24c65d96647678d114e04a2766df0caabaef12343', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5442, hash: '956a96d3e82fbc295e0eea1ff04067965705607950f4a619429569780dddd736', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-B4SNAQ6I.css': {size: 84966, hash: 'b3mpnjCDpdY', text: () => import('./assets-chunks/styles-B4SNAQ6I_css.mjs').then(m => m.default)}
   },
 };
