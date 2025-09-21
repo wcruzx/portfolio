8aff23ff7c269a50032143429c5fc4c49c3bfcdd
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-21 12:53:45 -0400

    ajustes na seção de education

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5232, hash: 'ad155352040b40a5434c31f9aec40420e905f6626ca57c54e09f25ff5264e636', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5422, hash: '542b2790fd713c10e570b6bd8a5a1b035413eb50647a012ed1d1abccebfb1d5d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 69103, hash: '5bbccba986a02c4baaebcf3ccaae790f8131ffe2ef6d0dcd460ca4229d18b477', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5232, hash: 'e0bb8fb4923e75ff633b836d84811b8c7651e0fdea587f986efd3c84d8ae46b4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5422, hash: '2b3711e89bb2bec501b555e7b3455936762a858f798e83b627a2d94460ddefb7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 69197, hash: '0a13dc0d619cc9ccbc7df7e25e53b42ccb647910d3c016282d2096c4ed974bea', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-B4SNAQ6I.css': {size: 84966, hash: 'b3mpnjCDpdY', text: () => import('./assets-chunks/styles-B4SNAQ6I_css.mjs').then(m => m.default)}
   },
 };
