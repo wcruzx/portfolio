d3933c40635260910b5fb8d65219ace9c6b4229b
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-21 12:14:39 -0400

    fix: rebuild para github pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,18 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'C:/Program Files/Git/portfolio/',
+  baseHref: '/portfolio/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Program%20Files/Git/portfolio"
+    "route": "/portfolio"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5252, hash: 'fd84451c5551aad766e3a3b24c65d96647678d114e04a2766df0caabaef12343', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 5442, hash: '956a96d3e82fbc295e0eea1ff04067965705607950f4a619429569780dddd736', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5232, hash: 'ad155352040b40a5434c31f9aec40420e905f6626ca57c54e09f25ff5264e636', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 5422, hash: '542b2790fd713c10e570b6bd8a5a1b035413eb50647a012ed1d1abccebfb1d5d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 69103, hash: '5bbccba986a02c4baaebcf3ccaae790f8131ffe2ef6d0dcd460ca4229d18b477', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-B4SNAQ6I.css': {size: 84966, hash: 'b3mpnjCDpdY', text: () => import('./assets-chunks/styles-B4SNAQ6I_css.mjs').then(m => m.default)}
   },
 };
