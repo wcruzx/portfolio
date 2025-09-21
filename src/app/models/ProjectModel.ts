b74c0a3f588db18852d874050273b18abd9753dd
Weverson Cruz <weversoncruzz@gmail.com>
2025-09-21 12:04:43 -0400

    atualizações de refinamento

--- src/app/models/ProjectModel.ts
+++ src/app/models/ProjectModel.ts
@@ -1,9 +1,11 @@
+import { SkillModel, SKILLS_DATA } from './SkillsModel';
+
 export interface ProjectModel {
   id: number;
-  image: string;
   title: string;
   description: string;
-  technologies: string[];
+  image: string;
+  technologies: SkillModel[];
   liveUrl?: string;
   githubUrl?: string;
 }
@@ -15,7 +17,10 @@ export const PROJECTS_DATA: ProjectModel[] = [
     title: 'Sistema de Empreendedorismo Jurídico',
     description:
       'Sistema com o objetivo de fomentar a inovação e a autonomia dos profissionais do direito.',
-    technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'PostgreSQL'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'Angular')!,
+      SKILLS_DATA.find((skill) => skill.name === 'TypeScript')!,
+    ],
     liveUrl: 'http://sej.oabms.org.br/',
     githubUrl: 'http://sej.oabms.org.br/',
   },
@@ -25,7 +30,14 @@ export const PROJECTS_DATA: ProjectModel[] = [
     title: 'Sistema Municipal de Gestão de Estágio',
     description:
       'Sistema para inscrição e gerenciamento de estágios da Prefeitura de Campo Grande, que oferece vagas remuneradas para estudantes de nível profissionalizante, médio e superior.',
-    technologies: ['Java', 'Angular', 'PostgreSQL', 'Oracle', 'HTML', 'CSS'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'HTML5')!,
+      SKILLS_DATA.find((skill) => skill.name === 'CSS3')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Angular')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Java')!,
+      SKILLS_DATA.find((skill) => skill.name === 'PostgreSQL')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Oracle')!,
+    ],
     liveUrl: 'https://estagio.campogrande.ms.gov.br/home',
     githubUrl: 'https://estagio.campogrande.ms.gov.br',
   },
@@ -35,7 +47,14 @@ export const PROJECTS_DATA: ProjectModel[] = [
     title: 'Sistema de Agendamento de Castração',
     description:
       'Sistema de agendamento de castração do município de Campo Grande para controle e marcação de procedimentos de castração animal.',
-    technologies: ['Java', 'Angular', 'PostgreSQL', 'Oracle', 'HTML', 'CSS'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'HTML5')!,
+      SKILLS_DATA.find((skill) => skill.name === 'CSS3')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Angular')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Java')!,
+      SKILLS_DATA.find((skill) => skill.name === 'PostgreSQL')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Oracle')!,
+    ],
     liveUrl: 'https://castracao.campogrande.ms.gov.br',
     githubUrl: 'https://castracao.campogrande.ms.gov.br',
   },
@@ -44,7 +63,13 @@ export const PROJECTS_DATA: ProjectModel[] = [
     image: 'assets/project4.png',
     title: 'Chess Game',
     description: 'O Chess Game é uma aplicação de xadrez multijogador através do navegador Web.',
-    technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'HTML5')!,
+      SKILLS_DATA.find((skill) => skill.name === 'CSS3')!,
+      SKILLS_DATA.find((skill) => skill.name === 'React')!,
+      SKILLS_DATA.find((skill) => skill.name === 'NodeJS')!,
+      SKILLS_DATA.find((skill) => skill.name === 'JavaScript')!,
+    ],
     liveUrl: 'https://github.com/wcruzx/chess-game',
     githubUrl: 'https://github.com/wcruzx/chess-game',
   },
@@ -54,7 +79,14 @@ export const PROJECTS_DATA: ProjectModel[] = [
     title: 'Nunes Sports',
     description:
       'Sistema de gerenciamento de produtos desenvolvido para um desafio técnico, ele permite a exibição, criação, edição e delete de produtos.',
-    technologies: ['HTML', 'CSS', 'React', 'BootStrap', 'Java', 'Spring', 'H2'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'HTML5')!,
+      SKILLS_DATA.find((skill) => skill.name === 'CSS3')!,
+      SKILLS_DATA.find((skill) => skill.name === 'React')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Java')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Spring')!,
+      SKILLS_DATA.find((skill) => skill.name === 'H2')!,
+    ],
     liveUrl: 'https://github.com/wcruzx/NunesSports',
     githubUrl: 'https://github.com/wcruzx/NunesSports',
   },
@@ -64,7 +96,12 @@ export const PROJECTS_DATA: ProjectModel[] = [
     title: 'API RESTful - ViaCEP',
     description:
       'O projeto é uma API RESTful, que consulta o endereço com base em um CEP fornecido e calcula o frete conforme a região do Brasil.',
-    technologies: ['Java', 'SpringBoot', 'Maven', 'Postman'],
+    technologies: [
+      SKILLS_DATA.find((skill) => skill.name === 'Java')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Spring')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Maven')!,
+      SKILLS_DATA.find((skill) => skill.name === 'Postman')!,
+    ],
     liveUrl: 'https://github.com/wcruzx/findcep',
     githubUrl: 'https://github.com/wcruzx/findcep',
   },
--- src/app/models/SkillsModel.ts
+++ src/app/models/SkillsModel.ts
@@ -1,6 +1,6 @@
 export interface SkillModel {
   name: string;
-  iconPath: string; // Path to the SVG file
+  iconPath: string;
   category?: string;
 }
 
@@ -8,76 +8,96 @@ export const SKILLS_DATA: SkillModel[] = [
   {
     name: 'Angular',
     iconPath: 'assets/angular.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: 'Bootstrap',
     iconPath: 'assets/bootstrap.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: 'C#',
     iconPath: 'assets/csharp.svg',
-    category: 'Backend'
+    category: 'Backend',
   },
   {
     name: 'CSS3',
     iconPath: 'assets/css3.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: '.NET Core',
     iconPath: 'assets/dotnetcore.svg',
-    category: 'Backend'
+    category: 'Backend',
   },
   {
     name: 'Git',
     iconPath: 'assets/git.svg',
-    category: 'Tools'
+    category: 'Tools',
   },
   {
     name: 'GitHub',
     iconPath: 'assets/github.svg',
-    category: 'Tools'
+    category: 'Tools',
   },
   {
     name: 'HTML5',
     iconPath: 'assets/html5.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: 'Java',
     iconPath: 'assets/java.svg',
-    category: 'Backend'
+    category: 'Backend',
   },
   {
     name: 'JavaScript',
     iconPath: 'assets/javascript.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: 'PostgreSQL',
     iconPath: 'assets/postgresql.svg',
-    category: 'Database'
+    category: 'Database',
+  },
+  {
+    name: 'Oracle',
+    iconPath: 'assets/oracle.svg',
+    category: 'Database',
+  },
+  {
+    name: 'H2',
+    iconPath: 'assets/h2.png',
+    category: 'Database',
   },
   {
     name: 'Postman',
     iconPath: 'assets/postman.svg',
-    category: 'Tools'
+    category: 'Tools',
   },
   {
     name: 'React',
     iconPath: 'assets/react.svg',
-    category: 'Frontend'
+    category: 'Frontend',
   },
   {
     name: 'Spring',
     iconPath: 'assets/spring.svg',
-    category: 'Backend'
+    category: 'Backend',
+  },
+  {
+    name: 'Maven',
+    iconPath: 'assets/maven.svg',
+    category: 'Backend',
+  },
+  {
+    name: 'NodeJS',
+    iconPath: 'assets/nodejs.svg',
+    category: 'Backend',
   },
   {
     name: 'TypeScript',
     iconPath: 'assets/typescript.svg',
-    category: 'Frontend'
-  }
+    category: 'Frontend',
+  },
 ];
